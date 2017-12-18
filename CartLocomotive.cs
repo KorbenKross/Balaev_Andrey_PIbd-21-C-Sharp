cb32f7ecfdcde230622fa0249fb0adcbb87dc475
KorbenKross <31703739+KorbenKross@users.noreply.github.com>
2017-12-18 21:01:51 +0400

    Add files via upload

---
+++ CartLocomotive.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class CartLocomotive : Locomotive
+    {
+        private bool anthen;
+        private bool roof;
+        private bool lightEngine;
+        private Color dopColor;
+
+        public CartLocomotive(int maxSpeed, int maxCountPassengers, double weight, Color color, bool anthen, bool roof, bool lightEngine, Color dopColor) : base(maxSpeed, maxCountPassengers, weight, color)
+        {
+            this.anthen = anthen;
+            this.roof = roof;
+            this.lightEngine = lightEngine;
+            this.dopColor = dopColor;
+        }
+
+        protected override void drawBaseLocomotive(Graphics g)
+        {
+            if (anthen)
+            {
+                Pen penB = new Pen(Color.Black);
+
+                Brush Coal = new SolidBrush(dopColor);
+                g.DrawLine(penB, startPosX, startPosY - 30, startPosX + 30, startPosY - 30);
+                g.DrawLine(penB, startPosX, startPosY - 30, startPosX + 20, startPosY);
+                g.DrawLine(penB, startPosX + 30, startPosY - 30, startPosX + 20, startPosY);
+
+                g.DrawLine(penB, startPosX + 50, startPosY - 30, startPosX + 80, startPosY - 30);
+                g.DrawLine(penB, startPosX + 50, startPosY - 30, startPosX + 70, startPosY);
+                g.DrawLine(penB, startPosX + 80, startPosY - 30, startPosX + 70, startPosY);
+            }
+
+
+
+            if (roof)
+            {
+                Pen penB = new Pen(Color.Black);
+                Brush Coal = new SolidBrush(dopColor);
+                g.DrawRectangle(penB, startPosX + 10, startPosY + 10, 80, 10);
+                g.FillRectangle(Coal, startPosX + 10, startPosY + 30, 80, 10);
+
+            }
+            if (lightEngine)
+            {
+                Pen penB = new Pen(Color.Black);
+                Brush Coal = new SolidBrush(Color.Green);
+                g.DrawRectangle(penB, startPosX + 10, startPosY - 5, 70, 10);
+                g.FillRectangle(Coal, startPosX + 10, startPosY - 5, 70, 10);
+
+            }
+            base.drawBaseLocomotive(g);          
+
+        }
+
+        public void setDopColor(Color color)
+        {
+            dopColor = color;
+        }
+
+
+    }
+}
--- Locomotive.cs
+++ Locomotive.cs
@@ -5,46 +5,124 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApplication9
+namespace Lab2
 {
-	public abstract class Locomotive : ITtransport
-	{
-		protected float startPosX;
-		protected float startPosY;
-		protected int countPassengers;
+    public class Locomotive : Vehicle
+    {
 
-		public virtual int MaxCountPassengers { protected set; get; }
-		public virtual int MaxSpeed { protected set; get; }
-        public virtual int MaxCapacityGenerator { protected set; get; }
+        public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 150)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 50;
+                }
+            }
+        }
+
+        public override int MaxCountPassengers
+        {
+            get
+            {
+                return base.MaxCountPassengers;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 0)
+                {
+                    base.MaxCountPassengers = value;
+                }
+                else
+                {
+                    base.MaxCountPassengers = 4;
+                }
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+
+            protected set
+            {
+
+                if (value > 300 && value < 1000)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 1000;
+                }
+            }
+        }
+
+        public Locomotive(int maxSpeed, int maxCountPassengers, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountPassengers = maxCountPassengers;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countPassengers = 0;
+            Random rand = new Random();
+            startPosX = rand.Next(400, 500);
+            startPosY = rand.Next(100, 200);
+        }
 
 
-        public Color ColorBody { protected set; get; }
+        public override void drawLocomotive(Graphics g)
+        {
+            drawBaseLocomotive(g);
+        }
 
-		public virtual double Weight { protected set; get; }
+        public override void moveLocomotive(Graphics g)
+        {
+            startPosX -= (MaxSpeed *50/(float)Weight)/(countPassengers==0?1:countPassengers);
+            drawLocomotive(g);
+        }
 
-		public abstract void moveCar(Graphics g);
+        protected virtual void drawBaseLocomotive(Graphics g)
+        {
 
-		public abstract void drawCar(Graphics g);
+            Pen penB = new Pen(Color.Blue);
+            Pen penR = new Pen(Color.Red);
+            Brush brushR = new SolidBrush(Color.Red);
+            Brush brushB = new SolidBrush(Color.Blue);
+            Brush brushRB = new SolidBrush(Color.Gray);
 
-		public void setPosition(int x, int y)
-		{
-			startPosX = x;
-			startPosY = y;
-		}
+            
+            g.FillEllipse(brushRB, startPosX + 10, startPosY + 30, 20, 20);
+            g.FillEllipse(brushRB, startPosX + 75, startPosY + 30, 20, 20);
 
-		public void loadPassenger(int count)
-		{
-			if (countPassengers + count < MaxCountPassengers)
-			{
-				countPassengers += count;
-			}
-		}
 
-		public int getPassenger()
-		{
-			int count = countPassengers;
-			countPassengers = 0;
-			return count;
-		}
-	}
+            Brush LokoBrush = new SolidBrush(ColorBody);
+            g.FillRectangle(LokoBrush, startPosX, startPosY, 100, 30);
+
+
+            g.DrawRectangle(penB, startPosX + 85, startPosY + 5, 20, 20);
+            g.DrawRectangle(penB, startPosX + 65, startPosY + 5, 10, 20);
+            g.DrawLine(penB, startPosX + 100, startPosY, startPosX + 105, startPosY + 5);
+            g.DrawLine(penB, startPosX + 10, startPosY + 40, startPosX + 30, startPosY + 40);
+            g.DrawLine(penB, startPosX + 75, startPosY + 40, startPosX + 95, startPosY + 40);
+            g.DrawLine(penB, startPosX - 5, startPosY + 20, startPosX, startPosY + 20);
+            g.DrawLine(penB, startPosX - 5, startPosY + 10, startPosX - 5, startPosY + 25);
+            g.DrawLine(penB, startPosX, startPosY + 10, startPosX + 10, startPosY);
+            g.DrawLine(penB, startPosX, startPosY + 25, startPosX + 100, startPosY + 25);
+        }
+
+    }
 }
