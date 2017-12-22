410959b5e67f0244b2da33ecdf3c6b490d031d6f
KorbenKross <31703739+KorbenKross@users.noreply.github.com>
2017-12-22 21:35:01 +0400

    Add files via upload

--- PasTrain.cs
+++ PasTrain.cs
@@ -5,32 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApplication9
+namespace Lab2
 {
-	class PasTrain : Train
-	{
-		private bool lightEngine;
-		private bool anthen;
-		private bool roof;
+    public class CartLocomotive : Locomotive
+    {
+        private bool anthen;
+        private bool roof;
+        private bool lightEngine;
+        private Color dopColor;
 
-		private Color dopColor;
+        public CartLocomotive(int maxSpeed, int maxCountPassengers, double weight, Color color, bool anthen, bool roof, bool lightEngine, Color dopColor) : base(maxSpeed, maxCountPassengers, weight, color)
+        {
+            this.anthen = anthen;
+            this.roof = roof;
+            this.lightEngine = lightEngine;
+            this.dopColor = dopColor;
+        }
 
-		public PasTrain(int maxSpeed, int maxCountPassenger, int maxCapacityGenerator, double weight, Color color, bool lightEngine, bool anthen, bool roof, Color dopColor) : base(maxSpeed, maxCountPassenger, maxCapacityGenerator,weight, color)
-		{
-			this.lightEngine = lightEngine;
-			this.anthen = anthen;
-			this.roof = roof;
-			this.dopColor = dopColor;
-		}
-
-		protected override void drawLoc(Graphics g) {
+        protected override void drawBaseLocomotive(Graphics g)
+        {
             if (anthen)
             {
-            Pen penB = new Pen(Color.Black);
+                Pen penB = new Pen(Color.Black);
 
                 Brush Coal = new SolidBrush(dopColor);
-                g.DrawLine(penB, startPosX, startPosY - 30, startPosX + 30, startPosY - 30); 
-                g.DrawLine(penB, startPosX, startPosY - 30, startPosX + 20, startPosY); 
+                g.DrawLine(penB, startPosX, startPosY - 30, startPosX + 30, startPosY - 30);
+                g.DrawLine(penB, startPosX, startPosY - 30, startPosX + 20, startPosY);
                 g.DrawLine(penB, startPosX + 30, startPosY - 30, startPosX + 20, startPosY);
 
                 g.DrawLine(penB, startPosX + 50, startPosY - 30, startPosX + 80, startPosY - 30);
@@ -38,7 +38,7 @@ namespace WindowsFormsApplication9
                 g.DrawLine(penB, startPosX + 80, startPosY - 30, startPosX + 70, startPosY);
             }
 
-         
+
 
             if (roof)
             {
@@ -56,8 +56,15 @@ namespace WindowsFormsApplication9
                 g.FillRectangle(Coal, startPosX + 10, startPosY - 5, 70, 10);
 
             }
-            base.drawLoc(g);
+            base.drawBaseLocomotive(g);          
 
-		}
-	}
+        }
+
+        public void setDopColor(Color color)
+        {
+            dopColor = color;
+        }
+
+
+    }
 }
